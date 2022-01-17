f64d627fc09f50e30f9b1bfd3f11401d4ca062c3
Srish Rachamalla <78019070+srishrachamalla7@users.noreply.github.com>
2022-01-17 11:36:03 +0530

    Predict and have fun with python

---
+++ Readmefirst.txt
@@ -0,0 +1,3 @@
+In line 8 give the folder name between the apostrophes('') 
+NOTE: I recommed to save the file in the same folder you save the code
+If you have any doubts check the example program after downloading it save the dogecoin.csv file in the same foleder you saved the code
---
+++ predictiondoge.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from autots import AutoTS
+sns.set()
+plt.style.use()
+data=pd.read("Dogecoin.csv")
+print("shape of the data set is",data.shape)
+print(data.head())
+plt.figure(figsize=(10, 4))
+plt.title("Price")
+plt.xlabel("Date")
+plt.ylabel("Close")
+plt.plot(data["Close"])
+plt.show()
+model = AutoTS(forecast_length=10, frequency='infer', ensemble='simple', drop_data_older_than_periods=200)
+model = model.fit(data, date_col='Date', value_col='Close', id_col=None)
+prediction = model.predict()
+forecast = prediction.forecast
+print(" Prediction")
+print(forecast)
